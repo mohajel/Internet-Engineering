c5cbcc7177f7427cdaefe917ecce0e4b55bae00b
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-02-24 13:20:43 +0330

    test: add testSearchRestaurantsByName_NotFound

--- CA1/src/test/java/com/github/mohajel/IE/CA1/MizdooniSearchRestaurantsByNameTest.java
+++ CA1/src/test/java/com/github/mohajel/IE/CA1/MizdooniSearchRestaurantsByNameTest.java
@@ -2,12 +2,15 @@ package com.github.mohajel.IE.CA1;
 
 import com.github.mohajel.IE.CA1.testUtils.TestRestaurantFactory;
 import com.github.mohajel.IE.CA1.testUtils.TestUserFactory;
+import com.github.mohajel.IE.CA1.utils.MizdooniError;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 
 public class MizdooniSearchRestaurantsByNameTest {
 
@@ -30,5 +33,19 @@ public class MizdooniSearchRestaurantsByNameTest {
         validRestaurant.remove("managerUsername");
         assertTrue(res.getBoolean("success"));
 //        assertEquals(res.getJSONObject("data"), validRestaurant);
+    }
+
+    @Test
+    public void testSearchRestaurantsByName_NotFound() {
+        JSONObject validManager = TestUserFactory.createSimpleManager("manager1", "manager1@gmail.com");
+        app.addUser(validManager);
+        JSONObject validRestaurant = TestRestaurantFactory.createSimpleRestaurant("restaurant1", "manager1");
+        app.addRestaurant(validRestaurant);
+
+        JSONObject res = app.searchRestaurantsByName(new JSONObject().put("name", "restaurant2"));
+
+        assertFalse(res.getBoolean("success"));
+        assertEquals(res.getJSONObject("data").getString("error"), MizdooniError.RESTAURANT_DOES_NOT_EXIST);
+
     }
 }
