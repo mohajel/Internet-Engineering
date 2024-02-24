bc199442e830a9916a11b4c92674147aeac50bef
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-02-24 13:10:44 +0330

    test: add MizdooniSearchRestaurantsByNameTest.java

---
+++ CA1/src/test/java/com/github/mohajel/IE/CA1/MizdooniSearchRestaurantsByNameTest.java
@@ -0,0 +1,34 @@
+package com.github.mohajel.IE.CA1;
+
+import com.github.mohajel.IE.CA1.testUtils.TestRestaurantFactory;
+import com.github.mohajel.IE.CA1.testUtils.TestUserFactory;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MizdooniSearchRestaurantsByNameTest {
+
+    private MizdooniApp app;
+
+    @Before
+    public void setUp() {
+        app = new MizdooniApp();
+    }
+
+    @Test
+    public void testSearchRestaurantsByName_Valid() {
+        JSONObject validManager = TestUserFactory.createSimpleManager("manager1", "manager1@gmail.com");
+        app.addUser(validManager);
+        JSONObject validRestaurant = TestRestaurantFactory.createSimpleRestaurant("restaurant1", "manager1");
+        app.addRestaurant(validRestaurant);
+
+        JSONObject res = app.searchRestaurantsByName(new JSONObject().put("name", "restaurant1"));
+
+        validRestaurant.remove("managerUsername");
+        assertTrue(res.getBoolean("success"));
+//        assertEquals(res.getJSONObject("data"), validRestaurant);
+    }
+}
