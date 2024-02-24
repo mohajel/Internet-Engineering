fad99ed24fc3e3cca868daaa930492f14adf0afd
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-02-24 07:37:25 +0330

    feat(models/Restaurant): check startTime/endTime is rounded in constructor

--- CA1/src/main/java/com/github/mohajel/IE/CA1/models/Restaurant.java
+++ CA1/src/main/java/com/github/mohajel/IE/CA1/models/Restaurant.java
@@ -1,6 +1,24 @@
 package com.github.mohajel.IE.CA1.models;
 
+import com.github.mohajel.IE.CA1.utils.MizdooniError;
+
 public class Restaurant {
+
+    public Restaurant(String name, String managerUserName, String type, Hour startTime, Hour endTime, String description, Address address) throws MizdooniError {
+        this.name = name;
+        this.managerUserName = managerUserName;
+        this.type = type;
+
+        if (!(startTime.isHourRounded() && endTime.isHourRounded())) {
+            throw new MizdooniError(MizdooniError.HOUR_IS_NOT_ROUND);
+        }
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+        this.address = address;
+
+    }
+
     public String name;
     public String managerUserName;
     public String type;
