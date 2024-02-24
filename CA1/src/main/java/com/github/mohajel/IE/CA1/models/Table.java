32d18e828950a83c378db09d038060324ba4a0f0
mohajel <msadegi007@yahoo.com>
2024-02-24 09:09:36 +0330

    feat: Table model updated

--- CA1/src/main/java/com/github/mohajel/IE/CA1/models/Table.java
+++ CA1/src/main/java/com/github/mohajel/IE/CA1/models/Table.java
@@ -1,7 +1,16 @@
 package com.github.mohajel.IE.CA1.models;
 
 public class Table {
+
+    Table(int id, int capacity, String restaurantName, String managerUserName) {
+        this.id = id;
+        this.capacity = capacity;
+        this.restaurantName = restaurantName;
+        this.managerUserName = managerUserName;
+    }
+
     public int id;
     public int capacity;
     public String restaurantName;
+    public String managerUserName;
 }
