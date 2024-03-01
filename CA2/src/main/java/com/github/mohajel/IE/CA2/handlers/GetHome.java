f7594c7ce651dea6a6066285e7677ff5d55bd0da
mohajel <msadegi007@yahoo.com>
2024-03-01 10:40:07 +0330

    feat: templates added

--- CA2/src/main/java/com/github/mohajel/IE/CA2/handlers/GetHome.java
+++ CA2/src/main/java/com/github/mohajel/IE/CA2/handlers/GetHome.java
@@ -1,4 +1,4 @@
-package com.github.mohajel.IE.CA2;
+package com.github.mohajel.IE.CA2.handlers;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "GetHome", urlPatterns = {"/"})
+public class GetHome extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().println("<h1>OK</h1>");
+        response.getWriter().println("<h1>HOME SWEET HOME</h1>");
     }
 }
