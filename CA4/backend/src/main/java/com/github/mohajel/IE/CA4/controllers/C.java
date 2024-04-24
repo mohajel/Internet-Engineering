b9c2f5ac8acaa28ea256beb65664ce54fb0c938f
mohajel <msadegi007@yahoo.com>
2024-04-24 18:59:50 +0330

    feat:initial communication between backend and frontend established

--- CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/C.java
+++ CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/C.java
@@ -1,16 +1,20 @@
 package com.github.mohajel.IE.CA4.controllers;
 
-
-
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class C {
 
-    @GetMapping("/")
+    @GetMapping("/api")
     String index() {
-        return "Hello, World!";
+        // return "Hello, World!";
+
+        JSONObject output = new JSONObject();
+        // output.put("success", true);
+        output.put("title", "hii");
+        return output.toString();
     }
 
 }
---
+++ CA4/frontend/client/src/App.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      // .then((data) => setData(data.message));
+      .then((data) => setData(data.title));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loadinggg..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
