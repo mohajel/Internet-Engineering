6748d8d1c493d710a81db5fa9153cc777fea6b49
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-04-29 10:17:48 +0330

    fix: back format json /restaurants

--- CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/RestaurantController.java
+++ CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.github.mohajel.IE.CA4.controllers;
 
 import com.github.mohajel.IE.CA4.MizdooniApp;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 
 import org.json.JSONArray;
@@ -18,9 +19,48 @@ public class RestaurantController {
 
     @GetMapping({"/", ""})
     ResponseEntity<String> getRestaurantsWithRating() {
-        MizdooniApp app = MizdooniApp.getInstance();
-        JSONArray restaurants = app.getAllRestaurantsWithAVGRate();
+//        MizdooniApp app = MizdooniApp.getInstance();
+//        JSONArray restaurants = app.getAllRestaurantsWithAVGRate();
+//
+//        return ResponseEntity.ok().body(restaurants.toString());
 
-        return ResponseEntity.ok().body(restaurants.toString());
+        JSONObject restaurant1 = new JSONObject();
+        restaurant1.put("numberOfStars", 5);
+        restaurant1.put("restaurantName", "KFC");
+        restaurant1.put("reviewCount", 100);
+        restaurant1.put("restaurantType", "Fast Food");
+        restaurant1.put("location", "Tehran");
+        restaurant1.put("openStatus", "Open");
+        restaurant1.put("durationInfo", "10:00 - 22:00");
+
+        JSONObject restaurant2 = new JSONObject();
+        restaurant2.put("numberOfStars", 4);
+        restaurant2.put("restaurantName", "McDonalds");
+        restaurant2.put("reviewCount", 200);
+        restaurant2.put("restaurantType", "Fast Food");
+        restaurant2.put("location", "Tehran");
+        restaurant2.put("openStatus", "Open");
+        restaurant2.put("durationInfo", "10:00 - 22:00");
+
+        JSONObject restaurant3 = new JSONObject();
+        restaurant3.put("numberOfStars", 3);
+        restaurant3.put("restaurantName", "Pizza Hut");
+        restaurant3.put("reviewCount", 300);
+        restaurant3.put("restaurantType", "Fast Food");
+        restaurant3.put("location", "Tehran");
+        restaurant3.put("openStatus", "Open");
+        restaurant3.put("durationInfo", "10:00 - 22:00");
+
+
+        // JSonArray
+        JSONArray topRestaurants = new JSONArray();
+        topRestaurants.put(restaurant1);
+        topRestaurants.put(restaurant2);
+        topRestaurants.put(restaurant3);
+        topRestaurants.put(restaurant1);
+        topRestaurants.put(restaurant2);
+        topRestaurants.put(restaurant3);
+
+        return ResponseEntity.ok().body(topRestaurants.toString());
     }
 }
