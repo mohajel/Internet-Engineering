4c9359ae59f22c2ffeea6494614464bdfac748a6
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-04-29 09:30:16 +0330

    refactor: remove extra API

--- CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/RestaurantController.java
+++ CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/RestaurantController.java
@@ -2,32 +2,15 @@ package com.github.mohajel.IE.CA4.controllers;
 
 import org.springframework.http.MediaType;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import com.github.mohajel.IE.CA4.models.Restaurant;
-
-import netscape.javascript.JSObject;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 @RestController
 public class RestaurantController {
@@ -137,26 +120,4 @@ public class RestaurantController {
 
         return topRestaurants.toString();
     }
-
-
-    // not working don't know why let it be for now
-    @RequestMapping(value = "/image/{name}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity getImageWithMediaType(@PathVariable(value = "name") String imageName) throws IOException {
-        logger.info("Request: " + "image");
-
-        Resource resource = new ClassPathResource("static/restaurants/restaurant1.png");
-
-        if (resource.exists()) {
-            return ResponseEntity.ok()
-                    .contentType(MediaType.IMAGE_JPEG) // Set the appropriate content type for your image
-                    .body(resource);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-
-        // InputStream in = getClass()
-        // .getResourceAsStream("./resources/static/restaurants/restaurant1.png");
-        // return IOUtils.toByteArray(in);
-    }
-
 }
