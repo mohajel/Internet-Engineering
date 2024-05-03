06ab8c1e050a5cf3494942a70b3956241d4bc506
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-05-03 14:06:55 +0330

    feat: 'restaurants/getSuggestions' added, but city is hardcode

--- CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/RestaurantController.java
+++ CA4/backend/src/main/java/com/github/mohajel/IE/CA4/controllers/RestaurantController.java
@@ -119,56 +119,10 @@ public class RestaurantController {
 
     @GetMapping("/suggested")
     ResponseEntity<String> getSuggestions() {
-        JSONObject restaurant1 = new JSONObject();
-        restaurant1.put("numberOfStars", 5);
-        restaurant1.put("restaurantName", "KFC");
-        restaurant1.put("reviewCount", 100);
-        restaurant1.put("restaurantType", "Fast Food");
-        restaurant1.put("location", "Tehran");
-        restaurant1.put("openStatus", "Open");
-        restaurant1.put("durationInfo", "10:00 - 22:00");
-        restaurant1.put("imgURL", "https://resizer.otstatic.com/v2/photos/xlarge/1/31676318.webp");
-        restaurant1.put("id", "1");
+         String city = "Pittsburgh";
+         MizdooniApp app = MizdooniApp.getInstance();
+         JSONArray restaurants = app.searchRestaurantCardsByCity(city);
 
-        
-
-        JSONObject restaurant2 = new JSONObject();
-        restaurant2.put("numberOfStars", 4);
-        restaurant2.put("restaurantName", "McDonalds");
-        restaurant2.put("reviewCount", 200);
-        restaurant2.put("restaurantType", "Fast Food");
-        restaurant2.put("location", "Tehran");
-        restaurant2.put("openStatus", "Open");
-        restaurant2.put("durationInfo", "10:00 - 22:00");
-        restaurant2.put("imgURL", "https://resizer.otstatic.com/v2/photos/xlarge/3/54305906.webp");
-        restaurant2.put("id", "2");
-
-
-        JSONObject restaurant3 = new JSONObject();
-        restaurant3.put("numberOfStars", 3);
-        restaurant3.put("restaurantName", "Pizza Hut");
-        restaurant3.put("reviewCount", 300);
-        restaurant3.put("restaurantType", "Fast Food");
-        restaurant3.put("location", "Tehran");
-        restaurant3.put("openStatus", "Open");
-        restaurant3.put("durationInfo", "10:00 - 22:00");
-        restaurant3.put("imgURL", "https://resizer.otstatic.com/v2/photos/xlarge/2/48899571.webp");
-        restaurant3.put("id", "3");
-
-
-
-        // JSonArray
-        JSONArray topRestaurants = new JSONArray();
-        topRestaurants.put(restaurant1);
-        topRestaurants.put(restaurant2);
-        topRestaurants.put(restaurant3);
-        topRestaurants.put(restaurant1);
-        topRestaurants.put(restaurant2);
-        topRestaurants.put(restaurant3);
-        topRestaurants.put(restaurant1);
-        topRestaurants.put(restaurant2);
-        topRestaurants.put(restaurant3);
-
-        return ResponseEntity.ok().body(topRestaurants.toString());
+         return ResponseEntity.ok().body(restaurants.toString());
     }
 }
