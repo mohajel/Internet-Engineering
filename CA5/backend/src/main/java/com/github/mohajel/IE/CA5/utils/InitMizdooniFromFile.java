52ed3fdc5641d48e20792b664f5fb242b4be602b
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-05-13 20:26:59 +0330

    refactor: reading from file for reserveTable

--- CA5/backend/src/main/java/com/github/mohajel/IE/CA5/utils/InitMizdooniFromFile.java
+++ CA5/backend/src/main/java/com/github/mohajel/IE/CA5/utils/InitMizdooniFromFile.java
@@ -6,17 +6,15 @@ import com.github.mohajel.IE.CA5.MizdooniApp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class InitMizdooniFromFile {
-    public static String input_file = "inputForInit.txt";
+    public static String input_file = "/backend/src/main/java/com/github/mohajel/IE/CA5/utils/inputForInit.txt";
     
     
 
     public static void init(MizdooniApp app) throws FileNotFoundException {
-        String currentDir = System.getProperty("user.dir") + "/backend/src/main/java/com/github/mohajel/IE/CA5/utils/inputForInit.txt";
-        System.out.println("Current dir using System:" +currentDir);
+        String currentDir = System.getProperty("user.dir") + input_file;
         File file = new File(currentDir);
         Scanner scanner = new Scanner(file);
 
@@ -33,31 +31,19 @@ public class InitMizdooniFromFile {
         String command = input.getString("command");
         JSONObject data = input.getJSONObject("data");
 
-        switch (command) {
-            case "addUser":
-                return app.addUser(data);
-            case "addRestaurant":
-                return app.addRestaurant(data);
-            case "addTable":
-                return app.addTable(data);
-            case "reserveTable":
-                return app.reserveTable(data);
-            case "cancelReservation":
-                return app.cancelReservation(data);
-            case "showReservationHistory":
-                return app.showReservationHistory(data);
-            case "searchRestaurantsByName":
-                return app.searchRestaurantsContainName(data);
-            case "searchRestaurantsByType":
-                return app.searchRestaurantsByType(data);
-            case "searchRestaurantsByCity":
-                return app.searchRestaurantsByCity(data);
-            case "showAvailableTables":
-                return app.showAvailableTables(data);
-            case "addReview":
-                return app.addReview(data);
-            default:
-                return new JSONObject().put("error", MizdooniError.INVALID_COMMAND);
-        }
+        return switch (command) {
+            case "addUser" -> app.addUser(data);
+            case "addRestaurant" -> app.addRestaurant(data);
+            case "addTable" -> app.addTable(data);
+            case "reserveTable" -> app.reserveTable(data);
+            case "cancelReservation" -> app.cancelReservation(data);
+            case "showReservationHistory" -> app.showReservationHistory(data);
+            case "searchRestaurantsByName" -> app.searchRestaurantsContainName(data);
+            case "searchRestaurantsByType" -> app.searchRestaurantsByType(data);
+            case "searchRestaurantsByCity" -> app.searchRestaurantsByCity(data);
+            case "showAvailableTables" -> app.showAvailableTables(data);
+            case "addReview" -> app.addReview(data);
+            default -> new JSONObject().put("error", MizdooniError.INVALID_COMMAND);
+        };
     }
 }
