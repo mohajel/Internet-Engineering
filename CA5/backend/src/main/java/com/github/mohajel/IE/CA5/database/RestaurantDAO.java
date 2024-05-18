1c1bd66045c189cf81625304b48fc06048e61590
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-05-18 14:32:19 +0330

    refactor: reformat

--- CA5/backend/src/main/java/com/github/mohajel/IE/CA5/database/RestaurantDAO.java
+++ CA5/backend/src/main/java/com/github/mohajel/IE/CA5/database/RestaurantDAO.java
@@ -2,7 +2,6 @@ package com.github.mohajel.IE.CA5.database;
 
 import com.github.mohajel.IE.CA5.models.Restaurant;
 import com.github.mohajel.IE.CA5.utils.HibernateDatabaseUtil;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 
 public class RestaurantDAO {
@@ -11,18 +10,19 @@ public class RestaurantDAO {
         return HibernateDatabaseUtil.getEntityManager();
     }
 
-    public static Restaurant findRestaurantByName(String name) {
-        EntityManager entityManager = getEntityManager();
-        Restaurant restaurant = entityManager.find(Restaurant.class, name);
-        entityManager.close();
-        return restaurant;
-    }
-
     public static void addRestaurant(Restaurant restaurant) {
         EntityManager entityManager = getEntityManager();
         entityManager.getTransaction().begin();
         entityManager.persist(restaurant);
         entityManager.getTransaction().commit();
         entityManager.close();
+    }
+
+    // Search
+    public static Restaurant findRestaurantByName(String name) {
+        EntityManager entityManager = getEntityManager();
+        Restaurant restaurant = entityManager.find(Restaurant.class, name);
+        entityManager.close();
+        return restaurant;
     }
 }
