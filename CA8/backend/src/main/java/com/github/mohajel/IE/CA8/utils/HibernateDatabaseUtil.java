fe0d5bcfba2ccc609871290cd80022a8d6f97ae9
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-06-10 14:53:53 +0330

    fix: finally connect to mysql

--- CA8/backend/src/main/java/com/github/mohajel/IE/CA8/utils/HibernateDatabaseUtil.java
+++ CA8/backend/src/main/java/com/github/mohajel/IE/CA8/utils/HibernateDatabaseUtil.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Persistence;
 import java.sql.*;
 
 public class HibernateDatabaseUtil {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306";
+    private static final String JDBC_URL = "jdbc:mysql://database:3306";
     private static final String DATABASE_NAME = "Mizdooni";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1234";
@@ -20,33 +20,33 @@ public class HibernateDatabaseUtil {
     }
 
     public static void createDataBase() {
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
+        // try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        //      Statement statement = connection.createStatement()) {
 
-            // Check if the database exists
-            String showDatabasesQuery = "SHOW DATABASES";
-            ResultSet resultSet = statement.executeQuery(showDatabasesQuery);
+        //     // Check if the database exists
+        //     String showDatabasesQuery = "SHOW DATABASES";
+        //     ResultSet resultSet = statement.executeQuery(showDatabasesQuery);
 
-            boolean databaseExists = false;
-            while (resultSet.next()) {
-                String databaseName = resultSet.getString(1);
-                if (DATABASE_NAME.equals(databaseName)) {
-                    databaseExists = true;
-                    break;
-                }
-            }
+        //     boolean databaseExists = false;
+        //     while (resultSet.next()) {
+        //         String databaseName = resultSet.getString(1);
+        //         if (DATABASE_NAME.equals(databaseName)) {
+        //             databaseExists = true;
+        //             break;
+        //         }
+        //     }
 
-            if (!databaseExists) {
-                // Database does not exist, create it
-                String createDatabaseQuery = "CREATE DATABASE " + DATABASE_NAME;
-                statement.executeUpdate(createDatabaseQuery);
-                System.out.println("Database " + DATABASE_NAME + " created successfully.");
-            } else {
-                System.out.println("Database " + DATABASE_NAME + " already exists.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        //     if (!databaseExists) {
+        //         // Database does not exist, create it
+        //         String createDatabaseQuery = "CREATE DATABASE " + DATABASE_NAME;
+        //         statement.executeUpdate(createDatabaseQuery);
+        //         System.out.println("Database " + DATABASE_NAME + " created successfully.");
+        //     } else {
+        //         System.out.println("Database " + DATABASE_NAME + " already exists.");
+        //     }
+        // } catch (SQLException e) {
+        //     e.printStackTrace();
+        // }
         initializeEntityManagerFactory();
     }
 
