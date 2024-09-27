5f272b8a1339fc54f6a782546c29ffd734c745b9
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2024-09-27 16:32:47 +0330

    refactor(CA5): enhance file structure and rename DemoApplication to Main

--- CA5/backend/src/main/java/com/github/mohajel/IE/CA5/Main.java
+++ CA5/backend/src/main/java/com/github/mohajel/IE/CA5/Main.java
@@ -1,19 +1,17 @@
-package com.github.mohajel.IE.CA5.controllers;
+package com.github.mohajel.IE.CA5;
 
 
 import java.util.Collections;
 
-import com.github.mohajel.IE.CA5.MizdooniApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(DemoApplication.class);
-        app.setDefaultProperties(Collections
-          .singletonMap("server.port", "8083"));
+		SpringApplication app = new SpringApplication(Main.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
 
 		// get the instance of the app for Init it
 		MizdooniApp.getInstance();
